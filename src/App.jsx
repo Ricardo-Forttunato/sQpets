38277740508e2af6c43705a451e735d879d3a5c1
luckfer7 <lucasferreira059@gmail.com>
2024-07-28 16:21:09 -0300

    imagem e texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import Homepage from './components/Homepage/Homepage';
+
 
 function App() {
 
   return (
     <>
-      <h1>Lucas</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />}></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,25 @@
+import './Homepage.scss';
+import imagem from '../../assets/imgs/ilustracao-capivara-homepage.svg'
+
+function Homepage () {
+    return (
+        <>
+            <header>s<span>Q</span>pets</header>
+
+            <section className='conteudo'>
+                <div className='ilustracao-capivara-homepage'>
+                    <img className='img' src={imagem} alt="imagem da capivara" />
+                </div>
+                <div className='texto__ancoras__container'>
+                    <p className='texto'>Alcançar seus objetivos ficou mais fácil e divertido !</p>
+                    <a>Cadastre-se</a> <a>Entrar</a>
+                </div>
+            </section>
+        </>
+    )
+        
+        
+}
+
+
+export default Homepage;
